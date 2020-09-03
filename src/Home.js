95650477cedc6e9192ec90a70a520a7723e070a6
amadrid512 <67016410+amadrid512@users.noreply.github.com>
2020-09-02 18:15:03 -0700

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Link } from "react-router-dom"
+//import { Link } from "react-router-dom"
 import MainMenu from "./components/MainMenu"
 import ListPage from "./components/ListPage"
-import { navigate } from "@reach/router";
+//import { navigate } from "@reach/router";
 
 
 export function Home() {
